91ccf399635f902759f9d8717427f9f54c72698d
Aluru Murali Krishna <alurumuralikrishna9@gmail.com>
2020-12-28 01:38:02 +0530

    Initial Commit

---
+++ tailwind.config.js
@@ -0,0 +1,23 @@
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      margin: {
+        7: '1.75rem',
+      },
+      colors: {
+        teal: {
+          DEFAULT: '#008080',
+        },
+      },
+    },
+    fontFamily: {
+      body: ['Inter', 'sans-serif'],
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+};
